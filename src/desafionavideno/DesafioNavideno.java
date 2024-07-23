123abfd903100e5e159a16d4ddd804bb3fbd2db8
Roli90 <rodrigoteran@gmail.com>
2024-07-23 12:10:57 -0300

    Interfaz y logica emogi terminada

--- src/desafionavideno/DesafioNavideno.java
+++ src/desafionavideno/DesafioNavideno.java
@@ -1,12 +1,19 @@
 
 package desafionavideno;
 
+import desafionavideno.igu.Principal;
+
 
 public class DesafioNavideno {
 
 
     public static void main(String[] args) {
-       
+        
+        Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+        
+        
     }
     
 }
---
+++ src/desafionavideno/logica/Controladora.java
@@ -0,0 +1,15 @@
+
+package desafionavideno.logica;
+
+public class Controladora {
+    
+    public int determinarCantidadEmoji(String emoji, int cantidad, String matriz[][]){
+        
+        return 0;
+    }
+    
+    public String[][]  generarMatriz(){
+        
+        return "";
+    }
+}
